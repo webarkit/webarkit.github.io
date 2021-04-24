4f30f5d116577b78c74e2f023d421d63eb00c632
kalwalt <github@kalwaltart.it>
2021-04-24 23:33:41 +0200

    fix for the manifest.json issue https://github.com/webarkit/webarkit.github.io/issues/31

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // set names for both precache & runtime cache
 workbox.core.setCacheNameDetails({
     prefix: 'webarkit-site',
-    suffix: 'v1',
+    suffix: 'v1.0',
     precache: 'precache',
     runtime: 'runtime-cache'
 });
